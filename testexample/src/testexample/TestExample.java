e0f8de646795af3fa7c1bd277d2188a24e061a56
dm-tigers <dm-tigers@users.noreply.github.com>
2017-04-06 21:03:29 -0400

    Update TestExample.java

--- testexample/src/testexample/TestExample.java
+++ testexample/src/testexample/TestExample.java
@@ -1,7 +1,9 @@
 package testexample;
 
-public class TestExample{
-	public static void main(String[] args) {
+public class TestExample
+{
+	public static void main(String[] args) 
+	{
 		rect myRect = new rect(5,3);
 		cuboid myCuboid = new cuboid(3,5,4);
 		myRect.printInformation();
