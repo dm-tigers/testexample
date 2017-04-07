a24b66a231a0ea0c043a833bc9bff3dc1ba20d69
dm-tigers <dm-tigers@users.noreply.github.com>
2017-04-06 21:02:37 -0400

    Update Shape2d.java

--- testexample/src/testexample/Shape2d.java
+++ testexample/src/testexample/Shape2d.java
@@ -1,7 +1,8 @@
 package testexample;
 
 
-public interface Shape2d extends ShapeBasic {
+public interface Shape2d extends ShapeBasic 
+{
 	double getUnit(s_unit unit);
 	void setUnit(s_unit unit, double value);
 	double getArea();
